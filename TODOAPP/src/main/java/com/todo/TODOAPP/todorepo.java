14bdefa59ec6b024cf3a820b09fe3fd2cb056486
Karthik27071999 <karthikkarnan17@gmail.com>
2024-02-23 12:33:08 +0530

    Updated spring_tasks

--- TODOAPP/src/main/java/com/todo/TODOAPP/todorepo.java
+++
@@ -1,8 +0,0 @@
-package com.todo.TODOAPP;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface todorepo extends JpaRepository<todoent, Long> {
-
-}
